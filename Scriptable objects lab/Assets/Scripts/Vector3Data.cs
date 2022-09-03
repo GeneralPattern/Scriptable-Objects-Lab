a5146a5b37d8842f56e53a28882bb10f84ce785c
Cory <Cannedcornflakes@gmail.com>
2022-09-02 18:41:06 -0600

    Added a Location Behavior script

--- Scriptable objects lab/Assets/Scripts/Vector3Data.cs
+++ Scriptable objects lab/Assets/Scripts/Vector3Data.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vector3Data : MonoBehaviour
+[CreateAssetMenu(fileName = "PickupLocation", menuName = "PickupLocation")]
+public class Vector3Data : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float locationX;
+    public float Locationy;
 }
